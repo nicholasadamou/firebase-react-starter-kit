e52be91e337f7fd93bfa3b3362c0de99da479813
Nicholas Adamou <nicholasadamouemail@gmail.com>
2020-05-26 09:17:38 -0400

    fixed issue with App.test.js

--- src/App.test.js
+++ src/App.test.js
@@ -1,11 +1,19 @@
 /* eslint-disable no-undef */
 /* eslint-disable no-unused-vars */
-import React from 'react'
-import ReactDOM from 'react-dom'
-import App from './App'
+import React from 'react';
+import ReactDOM from 'react-dom';
+import App from './App';
+
+import Firebase, { FirebaseContext } from './contexts/Firebase';
+
+const root = () => (
+    <FirebaseContext.Provider value={new Firebase()}>
+        <App />
+    </FirebaseContext.Provider>
+);
 
 it('renders without crashing', () => {
-  const div = document.createElement('div')
-  ReactDOM.render(<App />, div)
-  ReactDOM.unmountComponentAtNode(div)
-})
+    const div = document.createElement('div');
+    ReactDOM.render(root(), div);
+    ReactDOM.unmountComponentAtNode(div);
+});
