dc8e05d37426096bb9b640b17bb1c8c9ab55165e
Nicholas Adamou <nicholasadamouemail@gmail.com>
2020-05-26 03:00:25 -0400

    Update withAuthentication.js

--- src/contexts/Session/withAuthentication.js
+++ src/contexts/Session/withAuthentication.js
@@ -17,21 +17,19 @@ const withAuthentication = (Component) => {
         componentDidMount() {
             const { firebase } = this.props;
             
-            if (typeof firebase !== 'undefined' || firebase !== null) {
-                this.listener = firebase.onAuthUserListener(
-                    (authUser) => {
-                        if (typeof authUser !== 'undefined' || authUser !== null) {
-                            localStorage.setItem('authUser', JSON.stringify(authUser));
-                        }
-                    },
-                    () => {
-                        if (localStorage.hasOwnProperty('authUser')) {
-                            localStorage.removeItem('authUser');
-                        }
-                        this.setState({ authUser: null });
+            this.listener = firebase.onAuthUserListener(
+                (authUser) => {
+                    if (typeof authUser !== 'undefined' || authUser !== null) {
+                        localStorage.setItem('authUser', JSON.stringify(authUser));
                     }
-                );
-            }
+                },
+                () => {
+                    if (localStorage.hasOwnProperty('authUser')) {
+                        localStorage.removeItem('authUser');
+                    }
+                    this.setState({ authUser: null });
+                }
+            );
         }
 
         componentWillUnmount() {
